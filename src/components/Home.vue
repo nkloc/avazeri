6168a30e4965d0240f23b01a404038838cd57133
nkloc <56578888+nkloc@users.noreply.github.com>
2020-10-21 01:30:44 +0200

    installation

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Salut</h1>
+        <p>ça marche</p>
+        <h3>Allelouia</h3>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+@import '../css/variables'
+
+h1
+    font-family $font-title
+    margin-top 300px
+    font-size 30px
+p
+    font-family $font-content
+    padding 50px 0
+    font-size 30px
+h3
+    font-family $font-test
+    font-size 30px
+
+</style>
